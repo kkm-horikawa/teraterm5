6e7b8cf36280a89e424c5b7aa3dadd2efeb71f4a
kkm-horikawa <makoto10.devcre21@kkm-mako.com>
2024-08-14 03:47:15 +0900

    teraterm5.2 package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'teraterm5'
+$url = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.2/teraterm-5.2.exe' # Tera Term 5のインストーラーURL
+$installerType = 'exe'
+$silentArgs = '/verysilent /norestart'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
